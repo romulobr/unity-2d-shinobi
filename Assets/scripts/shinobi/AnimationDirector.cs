3e8eb6022e50a137626b1555592534fd558bb5a6
romulobr <romulobr@gmail.com>
2016-06-24 19:39:18 +0100

    fixes animations.

---
+++ Assets/scripts/shinobi/AnimationDirector.cs
@@ -0,0 +1,24 @@
+﻿namespace scripts.shinobi
+{
+    public class AnimationDirector
+    {
+        public string SelectAnimationFor(MovementInputReader.Movement movement, JumpInputReader.JumpState jump,
+            AttackInputReader.AttackState attack)
+        {
+            var animation = "idle";
+            if (attack == AttackInputReader.AttackState.Attacking)
+            {
+                animation = "attack";
+            }
+            else if (jump != JumpInputReader.JumpState.None)
+            {
+                animation = "jump";
+            }
+            else if (movement != MovementInputReader.Movement.None)
+            {
+                animation = "run";
+            }
+            return animation;
+        }
+    }
+}
--- Assets/scripts/shinobi/ShinobiController.cs
+++ Assets/scripts/shinobi/ShinobiController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Diagnostics;
 using UnityEngine;
+using UnityEngine.UI;
 using Debug = UnityEngine.Debug;
 
 namespace scripts.shinobi
@@ -10,16 +11,20 @@ namespace scripts.shinobi
         private readonly MovementInputReader movementInputReader;
         private readonly JumpInputReader jumpInputReader;
         private readonly AttackInputReader attackInputReader;
-        public float Speed = 0.5f;
-        public float JumpSpeed = 1.0f;
+        private readonly AnimationDirector animationDirector;
+
         private Animator animator;
         private SpriteRenderer spriteRenderer;
         private Rigidbody2D rigidBody2D;
-        public float JumpDuration = 1000f;
+        public Text debugText;
+
+        public float Speed = 0.5f;
+        public float JumpSpeed = 1.0f;
         private bool isGrounded = false;
 
         public ShinobiController()
         {
+            this.animationDirector = new AnimationDirector();
             this.attackInputReader = new AttackInputReader();
             this.jumpInputReader = new JumpInputReader();
             this.movementInputReader = new MovementInputReader();
@@ -32,33 +37,17 @@ namespace scripts.shinobi
             rigidBody2D = GetComponent<Rigidbody2D>();
         }
 
-        private void Walk()
-        {
-            if (isGrounded && !animator.GetCurrentAnimatorStateInfo(0).IsName("sword-attack"))
-            {
-                animator.Play("walking");
-                animator.SetBool("isWalking", true);
-            }
-        }
-
-        private void Idle()
-        {
-            if (isGrounded)
-            {
-                animator.Play("idle");
-                animator.SetBool("isWalking", false);
-            }
-        }
-
         private void HandleMovement(MovementInputReader.Movement movement)
         {
             if (movement == MovementInputReader.Movement.Right)
             {
+                spriteRenderer.flipX = false;
                 rigidBody2D.velocity = new Vector2(Speed, rigidBody2D.velocity.y);
             }
             else if (movement == MovementInputReader.Movement.Left)
             {
                 rigidBody2D.velocity = new Vector2(-Speed, rigidBody2D.velocity.y);
+                spriteRenderer.flipX = true;
             }
             else if (movement == MovementInputReader.Movement.None)
             {
@@ -66,35 +55,20 @@ namespace scripts.shinobi
             }
         }
 
-        private void HandleJumping(JumpInputReader.JumpState jumpState, float deltaTime)
+        private void HandleJumping(JumpInputReader.JumpState jumpState)
         {
-            if (jumpState.Equals(JumpInputReader.JumpState.Started))
+            if (jumpState == JumpInputReader.JumpState.Started)
             {
                 isGrounded = false;
                 rigidBody2D.velocity = new Vector2(rigidBody2D.velocity.x, JumpSpeed);
             }
         }
 
-        private bool IsAttacking()
-        {
-            return Input.GetButtonDown("Fire1") && !animator.GetCurrentAnimatorStateInfo(0).IsName("sword-attack") &&
-                   Input.GetAxis("Fire1") > 0;
-        }
-
-        private void OnCollisionEnter2D(Collision2D collision)
+        public void OnCollisionEnter2D(Collision2D collision)
         {
             if (rigidBody2D.velocity.y <= 0.1)
             {
                 jumpInputReader.ResetJumpState();
-            }
-        }
-
-        public void Update()
-        {
-            if (IsAttacking())
-            {
-                animator.Play("sword-attack");
-                animator.SetBool("isWalking", false);
             }
         }
 
@@ -104,13 +78,13 @@ namespace scripts.shinobi
             var movement = movementInputReader.Read();
             var jump = jumpInputReader.Read(deltaTime);
             var attack = attackInputReader.Read(deltaTime);
+            var selectedAnimation = animationDirector.SelectAnimationFor(movement, jump, attack);
 
-            Debug.Log(movement);
-            Debug.Log(jump);
-            Debug.Log(attack);
-
+            debugText.text = selectedAnimation+" > "+movement.ToString()+" / Jump"+jump.ToString() + " / " + attack.ToString();
             HandleMovement(movement);
-            HandleJumping(jump, deltaTime);
+            HandleJumping(jump);
+            
+            animator.Play(selectedAnimation);
         }
     }
 }
